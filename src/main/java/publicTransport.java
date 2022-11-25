b66988cbf5184afa53892b593d7c7deb9856d7f0
kyuung09 <kyuung09@gmail.com>
2022-11-25 19:10:16 +0900

    부모클래스(대중교통) 수정 - 주석 기입

--- src/main/java/publicTransport.java
+++ src/main/java/publicTransport.java
@@ -12,16 +12,21 @@ public class publicTransport {
     String status;
 
 
+    // 1. 버스 운행 시작시 상태를 출력하는 기능
     public void startBusStatus() {
         this.status = "운행중";
         System.out.println("|*.Alert" + this.status + "으로 상태가 변경 되었습니다.|");
     }
 
+    // 2. 버스 운행 종료시 상태를 출력하는 기능
     public void endStatus() {
         this.status = "차고지행";
         System.out.println("|*.Alert" + this.status + "으로 상태가 변경 되었습니다.|");
     }
 
+    // 3.주유기능
+    // 주유량을 입력받아 현재 주유량을 업데이트한다.
+    // 만약 주유량이 10 이하라면, 상태를 차고지행으로 변경한다.
     public void addOiling(int addOil) {
         this.oiling += addOil;
         if (oiling > 10) {
@@ -31,6 +36,10 @@ public class publicTransport {
             System.out.println("|*.Alert 주유 필요|");
         }
     }
+
+    // 4. 택시 | 시작시 주유량 확인 기능
+    // 운행시작 시 주유량을 확인하여 10 이상이면 주행가능상태("일반")으로 변경한다/
+    // 10이하면 탑승 불가하다.
     public void startCheckTax() {
         if (this.oiling > 10) {
             this.status = "일반";
@@ -40,6 +49,8 @@ public class publicTransport {
         }
     }
 
+    // 5. 현재 주유량, 누적요금, 상태를 알려주는 기능
+    // 현재 주유량이 0 이상일때는 주유량, 누적요금을 알려주고, 0 이하로 더이상 운행이 불가할때에는 상태까지 같이 보여준다.
     public void reportTax() {
         if ( this.oiling > 0) {
             System.out.println("|주유량|" + this.oiling + "|누적요금|" + this.totalPay + "|");
@@ -49,6 +60,9 @@ public class publicTransport {
         }
     }
 
+    // 6. 속도 변경 기능
+    // 현재 속도 확인 및 속도를 입력받아 속도를 변경하는 기능을 한다.
+    // 만약 현재 속도 보다 줄이고 싶은 속도의 수가 더 크면 Alert을 출력한다.
     public void checkSpeed(int changeSpeed) {
         if (this.oiling > 10) {
             if (this.speed + changeSpeed >= 0) {
@@ -62,6 +76,11 @@ public class publicTransport {
         }
     }
 
+    // 7.1 승객 탑승 기능(버스)
+    // 승객탑승 기능은 탑승할 승객 인원을 입력받아서 잔여 좌석이 있는지, 없는지 확인한다. (max = 30)
+    // 잔여 좌석이 있고, 현재 상태가 운행중이라면 nowPassenger에 탑승한 승객 수를 더한다.
+    // 버스의 경우 탑승료(boarding)가 1000원이므로, 승객수 * 기본 탑승료를 곱하여 총 합계 금액을 계산한다.
+    // 만일 잔여 좌석보다 신규 탑승자가 많으면 Alert을 발생시킨다.
     public void addPassenger(int addPassenger) {
         if (addPassenger + nowPassenger <= this.maxPassenger && status.equals("운행중")) {
             this.nowPassenger += addPassenger;
@@ -73,7 +92,10 @@ public class publicTransport {
             System.out.println("|*.Alert. 탑승자 초과|");
         }
     }
-
+    // 7.2 승객 탑승 기능(택시)
+    // 승객탑승 기능은 탑승할 승객 인원을 입력받아서 잔여 좌석이 있는지, 없는지 확인한다.
+    // 택시는 운행요금을 계산하는 기능을 추가하였다. 입력받은 운행거리가 기본 주행거리보다 길면, 거리를 계산하여 금액을 측정한다.
+    // 계산한 금액은 totalPay 변수에 넣어서 출력한다.
     public void addPassenger(int addPassenger, int distanceDriven) {
         if (addPassenger + nowPassenger < this.maxPassenger && status.equals("일반")) {
             this.status = "운행중";
