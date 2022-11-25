a5ea4269e830c543ed22e9df4088cf963fc76fad
kyuung09 <kyuung09@gmail.com>
2022-11-25 18:41:38 +0900

    부모클래스(대중교통) 수정 - 테스트용으로 사용한 main 문 제거

--- src/main/java/publicTransport.java
+++ src/main/java/publicTransport.java
@@ -100,16 +100,4 @@ public class publicTransport {
 
 
     }
-    public static void main(String[] args) {
-        publicTransport aTax = new Tax(2000);
-        aTax.startCheckTax();
-        aTax.addPassenger(2,2);
-        aTax.addOiling(-80);
-        aTax.reportTax();
-        aTax.addPassenger(5,2);
-        aTax.addPassenger(3,12);
-        aTax.addOiling(-20);
-        aTax.reportTax();
-    }
-
 }
